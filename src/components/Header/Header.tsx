3665d16c1cc25f6d6069637cbb1031a398d5c765
Raphael <raphael.araujo@estantemagica.com.br>
2020-09-20 13:49:31 -0300

    Ajustes no component Header.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+  
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import './Header.scss'
+
+export const Header = () => {
+    return (
+        <nav className="navbar">
+            <section className="sec-1"></section>
+            <section className="sec-2"></section>
+            <section className="sec-3">
+                <button type="button" className="signin">
+                    <Link to="">login</Link>
+                </button>
+                <button type="button" className="signup">
+                    <Link to="">cadastre-se</Link>
+                </button>
+            </section>
+        </nav>
+    )
+}
