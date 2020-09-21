d0b7959b06b0c9aa919c84518569aeb0e6696621
Raphael <raphael.araujo@estantemagica.com.br>
2020-09-20 22:30:33 -0300

    Commit.

--- src/pages/Donation/index.tsx
+++ src/pages/Donation/index.tsx
@@ -1,19 +1,49 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
+import { MdCreditCard } from 'react-icons/md';
+import Swal from 'sweetalert2'
+
+import api from '../../services/api'
 
 import './styles.scss'
 
 export default() => {
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const link = 'https://tmj-hackaton.herokuapp.com/ong'
+        const body = { mensagem: `Obrigado pela sua doação! Agora, para continuar acompanhando o resultado das suas doações, acesse esse link: ${link}.` }
+        
+        try {
+        
+            await api.post('/zenvia/viaWhats', body);
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Doação enviada com sucesso!'
+            })
+
+        } catch (error) {
+            console.log(error)
+            
+            Swal.fire({
+                icon: 'error',
+                title: 'Falha no servidor. Tente novamente mais tarde.'
+            })
+        }
+    } 
+
 	return (
         <main className="donation">
             <h1>Doar para ONG X</h1>
             <span>Escolha o método de pagamento a seguir</span>
 
-            <section>
-                <div>cartão</div>
-                <div>paypal</div>
+            <section className="buttons-container">
+                <button><MdCreditCard /></button>
+                <button>PayPal</button>
             </section>
             <section>
-                <form>
+                <form onSubmit={handleSubmit} className="col-2">
                     <section className="ong">
                         <h3>Informações de cobrança</h3>
 
@@ -27,21 +57,25 @@ export default() => {
                             <input type="text" name="" id=""/>
                         </div>
 
-                        <div className="input-block">
-                            <label htmlFor="email"> CIDADE </label>
-                            <input type="text" name="" id=""/>
+                        <div className="input-group">
+                            <div className="input-block">
+                                <label htmlFor="email"> CIDADE </label>
+                                <input type="text" name="" id=""/>
+                            </div>
 
-                            <label htmlFor="email"> CEP </label>
-                            <input type="text" name="" id=""/>
+                            <div className="input-block">
+                                <label htmlFor="email"> CEP </label>
+                                <input type="text" name="" id=""/>
+                            </div>
                         </div>
 
-                        <div className="input-block">
-                            <div>
+                        <div className="input-group">
+                            <div className="input-block" style={{marginBottom: 0}}>
                                 <label htmlFor="email"> ESTADO </label>
                                 <input type="text" name="" id=""/>
                             </div>
 
-                            <div>
+                            <div className="input-block" style={{marginBottom: 0}}>
                                 <label htmlFor="email"> PAÍS </label>
                                 <input type="text" name="" id=""/>
                             </div>
@@ -73,7 +107,7 @@ export default() => {
                             <input type="text" name="" id=""/>
                         </div>
 
-                        <button type="submit" className="purple-button">doar</button>
+                        <button type="submit" className="purple-button" title="Clique para doar" style={{marginTop: '1.9em', marginLeft: '0', width: '100%'}}>doar</button>
                     </section>
                 </form>
             </section>
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import { Header } from './components/Header/Header'
 import Homepage from './pages/Homepage/'
 import OngList from './pages/OngList/'
 import OngDetail from './pages/OngDetail/'
+import OngChange from './pages/OngChange'
 import Register from './pages/Register/'
 import Login from './pages/Login/'
 import Donation from './pages/Donation/'
@@ -17,7 +18,8 @@ const App = () => {
   		<Header />
 			<Switch>
 				<Route path='/ongs' exact component={ OngList } />
-				<Route path='/ong' component={ OngDetail } />
+				<Route path='/ong' exact component={ OngDetail } />
+				<Route path='/ong/change' component={ OngChange } />
 				<Route path='/register' component={ Register } />
 				<Route path='/login' component={ Login } />
 				<Route path='/donation' component={ Donation } />
